710f7da243b632a4ba2b60cad1390d57ddfdde8f
stumpy5 <waynecathc@hotmail.com>
2019-11-28 23:21:43 +0000

    added component

---
+++ _Dev/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Components
+import Clients from "./components/clients/clients";
+
+
+const App = () => {
+    return (
+        <div>
+            <h1>test is this your name</h1>
+            <Clients clients={["wayne", "Cath" ]} name={"Joe"}/>
+            {/* <Products products={[{name:"Flake", price: 0.99},{name:"Kit-Kat", price:1.29}]}/> */}
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
