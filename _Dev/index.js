bdc7f5ffbaa437e61f6dbd3d07a4008f973b7ebe
stumpy5 <waynecathc@hotmail.com>
2019-12-18 22:07:56 +0000

    Tidy up

--- _Dev/index.js
+++ _Dev/index.js
@@ -2,15 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Components
-import Clients from "./components/clients/clients";
+
+
 
 
 const App = () => {
     return (
         <div>
-            <h1>test is this your name</h1>
-            <Clients clients={["wayne", "Cath" ]} name={"Joe"}/>
-            {/* <Products products={[{name:"Flake", price: 0.99},{name:"Kit-Kat", price:1.29}]}/> */}
+            <h1>React Template</h1>
         </div>
     );
 }
